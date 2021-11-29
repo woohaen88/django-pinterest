2a1bc60a3e751c52dcc6a3f71054566a20cd708d
woohaen88 <woohaen88@gmail.com>
2021-11-29 14:54:03 +0000

    Signup implementation with CreateView

--- accountapp/views.py
+++ accountapp/views.py
@@ -2,7 +2,10 @@ from django.http.response import HttpResponseRedirect
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import HelloWorld
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
+from django.views.generic import CreateView
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 def hello_world(request):
@@ -19,3 +22,10 @@ def hello_world(request):
     else:
         hello_world_list = HelloWorld.objects.all()
         return render(request, 'accountapp/hello_world.html', context={'hello_world_list' : hello_world_list})
+
+
+class AccountCreateView(CreateView):
+    model = User
+    form_class = UserCreationForm  
+    success_url = reverse_lazy('accountapp:hello_world')
+    template_name = 'accountapp/create.html'
