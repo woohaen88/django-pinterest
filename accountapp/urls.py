595370b9f0c84dec8184a6b2fec0689740d5f2be
woohaen88 <woohaen88@gmail.com>
2021-11-29 15:53:05 +0000

    탈퇴 구현

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import hello_world, AccountCreateView, AccountDetailView, AccountUpdateView
+from .views import hello_world, AccountCreateView, AccountDetailView, AccountUpdateView, AccountDeleteView
 from django.contrib.auth.views import LoginView, LogoutView
 
 
@@ -12,4 +12,5 @@ urlpatterns = [
     path('create/', AccountCreateView.as_view(), name='create'),
     path('detail/<int:pk>', AccountDetailView.as_view(), name='detail'),
     path('update/<int:pk>', AccountUpdateView.as_view(), name='update'),
+    path('delete/<int:pk>', AccountDeleteView.as_view(), name='delete'),
 ]
